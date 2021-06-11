9ddb904bad0d758590cb412004f7d5194db134a7
omardevm <omardguez.contacto@gmail.com>
2021-06-11 16:47:52 -0500

    base articles

--- app/src/main/java/com/example/dualidad/fragments/ArticlesFragment.kt
+++ app/src/main/java/com/example/dualidad/fragments/ArticlesFragment.kt
@@ -1,21 +1,79 @@
 package com.example.dualidad.fragments
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.dualidad.R
+import com.example.dualidad.adapters.VideosAdapter
+import com.example.dualidad.databinding.FragmentArticlesBinding
+import com.example.dualidad.databinding.FragmentVideoBinding
+import com.example.dualidad.models.Measure
+import org.json.JSONException
+import org.json.JSONObject
+import java.io.IOException
+import java.io.InputStream
 
-class ArticlesFragment : Fragment() {
+class ArticlesFragment : Fragment(R.layout.fragment_articles) {
 
+    private lateinit var binding: FragmentArticlesBinding
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_articles, container, false)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentArticlesBinding.bind(view)
+        setUI()
+    }
+
+    private fun setUI() {
+        val dataSet = loadList()
+        binding.recyclerViewArticle.apply {
+            setHasFixedSize(true)
+            adapter = VideosAdapter(dataSet) {
+                //openUri
+                startActivity(Intent(Intent.ACTION_VIEW).setData(Uri.parse(it.url)))
+            }
+        }
+
+    }
+
+    private fun loadList(): List<Measure> {
+        val formList = arrayListOf<Measure>()
+        try {
+            val obj = JSONObject(loadJSONFromAsset() ?: "")
+            val arr = obj.getJSONArray("articles")
+            for (i in 0 until arr.length()) {
+                val inside = arr.getJSONObject(i)
+                formList.add(
+                    Measure(
+                        inside.getString("url"),
+                        inside.getString("title"),
+                        inside.getString("desc")
+                    )
+                )
+            }
+        } catch (e: JSONException) {
+            e.printStackTrace()
+        } finally {
+            return formList
+        }
+    }
+
+    private fun loadJSONFromAsset(): String? {
+        return try {
+            val `is`: InputStream = requireActivity().assets.open("mediums.json")
+            val size: Int = `is`.available()
+            val buffer = ByteArray(size)
+            `is`.read(buffer)
+            `is`.close()
+            String(buffer)
+        } catch (ex: IOException) {
+            ex.printStackTrace()
+            return null
+        }
     }
 
 }
--- app/src/main/java/com/example/dualidad/fragments/DetailsFragment.kt
+++ app/src/main/java/com/example/dualidad/fragments/DetailsFragment.kt
@@ -2,15 +2,18 @@ package com.example.dualidad.fragments
 
 import android.os.Bundle
 import android.util.Log
+import android.view.InflateException
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
+import com.example.dualidad.MainActivity
 import com.example.dualidad.R
 import com.example.dualidad.databinding.FragmentDetailsBinding
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.AbstractYouTubePlayerListener
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.views.YouTubePlayerView
 import java.io.IOException
+import java.lang.Exception
 
 
 class DetailsFragment : Fragment(R.layout.fragment_details) {
@@ -22,19 +25,27 @@ class DetailsFragment : Fragment(R.layout.fragment_details) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentDetailsBinding.bind(view)
-        youTubePlayerView = binding.youtubePlayerView
-        lifecycle.addObserver(youTubePlayerView)
         setVideo()
     }
 
     private fun setVideo() {
         try {
+            binding.apply {
+                title.text = args.video.title
+                desc.text = args.video.desc
+            }
+            youTubePlayerView = binding.youtubePlayerView
+            lifecycle.addObserver(youTubePlayerView)
             youTubePlayerView.addYouTubePlayerListener(object : AbstractYouTubePlayerListener() {
                 override fun onReady(youTubePlayer: YouTubePlayer) {
                     youTubePlayer.loadVideo(args.video.url, 0F)
                 }
             })
         } catch (e: IOException) {
+            Log.e("error", "${e.stackTrace}")
+        } catch (e: Exception) {
+            Log.e("error", "${e.stackTrace}")
+        } catch (e: InflateException) {
             Log.e("error", "${e.stackTrace}")
         }
     }
